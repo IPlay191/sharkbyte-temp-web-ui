6bcf335d962877cdcb4a3418d8cd70081c4d132e
Oliver Martinez Fernandez <olivermartinez0724@gmail.com>
2026-07-30 16:51:05 -0400

    The Location Page
    Added the location page and put it after the statistics page.

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar'
 import Hero from './sections/Hero'
 import Statistics from './sections/Statistics'
+import Location from './sections/Location'
 import ComingSoon from './sections/ComingSoon'
 import Sponsors from './sections/Sponsors'
 import Socials from './sections/Socials'
@@ -15,20 +16,17 @@ export default function Home() {
       {/* --- STANDARD VERTICAL SCROLLING --- */}
       <Hero />
       <Statistics />
+      <Location />
 
       {/* --- MODULAR GSAP HORIZONTAL SCROLLING --- */}
       <HorizontalScroll panels={2}>
 
         <div className="w-[100vw] h-svh flex-shrink-0">
-          
           <ComingSoon />
-
         </div>
 
         <div className="w-[100vw] h-svh flex-shrink-0">
-
           <Sponsors />
-
         </div>
 
       </HorizontalScroll>
