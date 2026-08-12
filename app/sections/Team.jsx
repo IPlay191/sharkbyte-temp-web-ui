8ac2ec676bae569d09e35fdec043a1d370675b9d
IPlay191 <jimmyjb006@gmail.com>
2026-08-12 15:43:31 -0400

    Updated Team Section
    - Added all current Community members in the section.
    - Added Prof. Bucher to the faculty advisors section.

--- app/sections/Team.jsx
+++ app/sections/Team.jsx
@@ -5,6 +5,7 @@ import {useRef, useEffect, useState } from "react";
 const Team = () => {
  const [showCarousel, setShowCarousel] = useState(true);
  const [showCMembers, setCMembers] = useState(true);
+
  const teamMembers = [
    {
      name: "Jimmy Jean Baptiste",
@@ -39,8 +40,8 @@ const Team = () => {
    {
      name: "Mikhail Guevara",
      role: "Marketing",
-     image: "https://placehold.net/avatar.svg",
-     linkedin: "https://www.linkedin.com/in/kathween-vargas-villafuerte-6409941aa/"
+     image: "https://i.ibb.co/hR4j6K5L/image.png",
+     linkedin: "https://www.linkedin.com/in/mikhail-guevara-a425a6231/"
    },
    {
      name: "Linet Lima",
@@ -86,10 +87,49 @@ const Team = () => {
    }
  ]
 
+  const communityMembers = [
+  {
+    name: "City of Coral Gables",
+    logo: "https://i.ibb.co/8LwsNNcX/image.png",
+    website: "https://www.coralgables.com/department/innovation-and-technology"
+  },
+  {
+    name: "MDC Entec",
+    logo: "https://i.ibb.co/ZRGcKBQz/image.png",
+    website: "https://www.mdc.edu/entec/"
+  }, 
+  {
+    name: "MDC Magic Lab",
+    logo: "https://i.ibb.co/hRjk6hwb/image.png",
+    website: "https://magic.mdc.edu/"
+  },
+  {
+    name: "Placeholder",
+    logo: "https://placehold.net/avatar.svg",
+    website: ""
+  },
+  {
+    name: "Placeholder",
+    logo: "https://placehold.net/avatar.svg",
+    website: ""
+  }, 
+  {
+    name: "Placeholder",
+    logo: "https://placehold.net/avatar.svg",
+    website: ""
+  } 
+ ]
+ const facultyAdvisors = [
+  {
+    name: "The literal goat Prof.Bucher",
+    image: "https://placehold.net/avatar.svg",
+    linkedin: "https://www.linkedin.com/in/carmen-bucher-55bb6538/",
+  }
+ ]
  // Split team members into two halves
  const firstHalf = teamMembers.slice(0, Math.ceil(teamMembers.length / 2)); // First 7 members
  const secondHalf = teamMembers.slice(Math.ceil(teamMembers.length / 2)); // Last 6 members
-
+ // TEAM MEMBER CARD INFO CONTAINER
  const TeamCard = ({ member }) => {
    return (
      <a 
@@ -106,6 +146,46 @@ const Team = () => {
          />
          <h3 className="font-bold tracking-wide text-[25px]">{member.name}</h3>
          <p className="text-gray-300 relative top-[-10px] text-[20px]">{member.role}</p>
+       </div>
+     </a>
+   );
+ };
+// COMMUNITY MEMBER INFO CONTAINER
+  const CommunityMemberCard = ({ member }) => {
+   return (
+     <a 
+       href={member.website} 
+       target="_blank" 
+       rel="noopener noreferrer"
+       className="my-1.5 flex justify-center items-center gap-0 flex-col bg-gradient-to-b from-gray-950 via-gray-900 to-gray-950 text-white px-2 py-1 border-2 border-gray-600 pixel-shadow text-center transition-transform duration-300 hover:animate-[wiggle_2s_ease-in-out_infinite] hover:scale-105 cursor-pointer flex-shrink-0 w-[20vw] h-[20vw]"
+     >
+       <div className=" h-[20vw]">
+         <img 
+           src={member.logo} 
+           alt={member.name}
+           className="w-[12vw] h-[12vw] rounded-full mx-auto mb-5 mt-0.5 border border-gray-600"
+         />
+         <h3 className="font-bold tracking-wide text-[25px]">{member.name}</h3>
+       </div>
+     </a>
+   );
+ };
+ //
+   const FacultyAdvisorCard = ({ member }) => {
+   return (
+     <a 
+       href={member.linkedin} 
+       target="_blank" 
+       rel="noopener noreferrer"
+       className="my-1.5 flex justify-center items-center gap-0 flex-col bg-gradient-to-b from-gray-950 via-gray-900 to-gray-950 text-white px-2 py-1 border-2 border-gray-600 pixel-shadow text-center transition-transform duration-300 hover:animate-[wiggle_2s_ease-in-out_infinite] hover:scale-105 cursor-pointer flex-shrink-0 w-[20vw] h-[20vw]"
+     >
+       <div className=" h-[20vw]">
+         <img 
+           src={member.image} 
+           alt={member.name}
+           className="w-[12vw] h-[12vw] rounded-full mx-auto mb-5 mt-0.5 border border-gray-600"
+         />
+         <h3 className="font-bold tracking-wide text-[25px]">{member.name}</h3>
        </div>
      </a>
    );
@@ -140,15 +220,19 @@ const Team = () => {
 
     {/*COMMUNITY MEMBERS BOX*/}
     {showCarousel == false && showCMembers == true &&(
-      <div className= "w-8/10 h-screen mx-10 bg-gradient-to-b from-gray-950 via-gray-900 to-gray-950 text-white px-4 py-2 border-3 border-gray-600 pixel-shadow"> 
-        Community members section lolololol
+      <div className= "w-8/10 grid grid-cols-3 gap-4 h-screen m-2 justify-items-center bg-gradient-to-b from-gray-950 via-gray-900 to-gray-950 text-white px-4 py-2 border-3 border-gray-600 pixel-shadow"> 
+          {communityMembers.map((member,index) => (
+            <CommunityMemberCard key = {index} member = {member}/>
+          ))}
       </div>
     )}
 
     {/*FALCULTY ADVISORS BOX*/}
     {showCarousel == false && showCMembers == false &&(
-      <div className= "w-8/10 h-screen mx-10 bg-gradient-to-b from-gray-950 via-gray-900 to-gray-950 text-white px-4 py-2 border-3 border-gray-600 pixel-shadow"> 
-        This is falculty lol
+      <div className= "w-8/10 grind grid-cols-2 justify-items-center content-center h-screen m-2 bg-gradient-to-b from-gray-950 via-gray-900 to-gray-950 text-white px-4 py-2 border-3 border-gray-600 pixel-shadow"> 
+        {facultyAdvisors.map((member,index) => (
+            <FacultyAdvisorCard key = {index} member = {member}/>
+          ))}
       </div>
     )}
     
