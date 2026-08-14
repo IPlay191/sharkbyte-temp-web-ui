d42acceaeb7985206fce5b63b992756df2650e15
IPlay191 <jimmyjb006@gmail.com>
2026-08-13 20:38:48 -0400

    Universal Page.js
    - Made a universal Page.js for everybody.

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar'
 import Hero from './sections/Hero'
 import Statistics from './sections/Statistics'
-import ComingSoon from './sections/ComingSoon'
 import Sponsors from './sections/Sponsors'
 import Socials from './sections/Socials'
 import HorizontalScroll from './components/HorizontalScroll'
 import Team from './sections/Team'
+import Location from './sections/Location'
+import About from './sections/About'
+import FAQ from './sections/FAQ'
 
 export default function Home() {
   return (
@@ -15,27 +17,28 @@ export default function Home() {
 
       {/* --- STANDARD VERTICAL SCROLLING --- */}
       <Hero />
+      <About />
       <Statistics />
+      <Location />
 
       {/* --- MODULAR GSAP HORIZONTAL SCROLLING --- */}
       <HorizontalScroll panels={2}>
-
         <div className="w-[100vw] h-svh flex-shrink-0">
           
-          <ComingSoon />
+          <Sponsors />
 
         </div>
-
+        
         <div className="w-[100vw] h-svh flex-shrink-0">
-
-          <Sponsors />
+          
+          <Team />
 
         </div>
 
       </HorizontalScroll>
 
       {/* --- BACK TO VERTICAL SCROLLING --- */}
-      <Team />
+      <FAQ />
       <Socials />
 
     </main>
